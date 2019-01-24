dab1c604afd9c900d242cc78ebf779bc88c56720
Alexandre NICOL <nemokervi@yahoo.fr>
2019-01-24 21:23:50 +1100

    Add 2 recipes

--- public/src/assets/json/demo_recipe.ts
+++ public/src/assets/json/demo_recipe.ts
@@ -26,7 +26,7 @@ export const RECIPES = [
 				order: 4,
 			},
 			{
-				name: '¾ pound pasta, any type',
+				name: '400g pasta, any type',
 				order: 5,
 			},
 			{
@@ -41,11 +41,11 @@ export const RECIPES = [
 		preparation: [
 			{
 				step: 'Bring a large pot of water to a boil. Meanwhile, in a large bowl or pasta bowl, combine the herbs, lemon zest, garlic, lemon juice and olive oil.',
-				order: 1
+				order: 0
 			},
 			{
 				step: 'When the water comes to a boil, salt generously and add the pasta. Follow the cooking instructions on the package, but check the pasta a minute before the indicated time. A few minutes before the pasta is done, add the peas to the water. When the pasta is just about al dente, remove a half cup of the cooking water and add to the bowl with the herbs. Drain the pasta and peas, toss with the herb mixture and the cheese, and serve.',
-				order: 2
+				order: 1
 			}
 		],
 		tips: 'Advance preparation: The herbs can be chopped several hours ahead, but don’t combine the ingredients until you’ve put the water on for the pasta.',
@@ -91,15 +91,15 @@ export const RECIPES = [
 		preparation: [
 			{
 				step: 'Heat oven to 425 degrees. In a large bowl, combine all the ingredients and use your hands to gently mix.',
-				order: 1
+				order: 0
 			},
 			{
 				step: 'Shape the meat into 12 golf-ball-size rounds (about 2 inches in diameter) and arrange on a greased rimmed baking sheet.',
-				order: 2
+				order: 1
 			},
 			{
 				step: 'Bake until golden and cooked through, about 15 minutes. Serve warm.',
-				order: 3
+				order: 2
 			}
 		],
 		tips: 'Leftover meatballs freeze well and can be reheated in the oven at 375 degrees until warmed through (about 20 minutes).',
@@ -152,23 +152,23 @@ export const RECIPES = [
 		preparation: [
 			{
 				step: 'Bring a large pot of salted water to a boil and blanch the cauliflower until tender, about 5 minutes. Drain it then left it out on a towel in the stove for about 30 minutes. (Switch on the stove for 5 minutes prior to help the eva poration)',
-				order: 1
+				order: 0
 			},
 			{
 				step: 'Heat oven to 200 degrees C and butter a 2-quart glass baking dish.',
-				order: 2
+				order: 1
 			},
 			{
 				step: 'In a small saucepan, melt the 2 tablespoons butter over medium heat. Stir in the flour until it forms a pale-golden paste, about 1 to 2 minutes. Stir in the heavy cream and bring the mixture to a low simmer, stirring frequently, until it thickens, about 2 to 3 minutes. Off heat, stir in half the Gruyère and fontina.',
-				order: 3
+				order: 2
 			},
 			{
 				step: 'Pat the cauliflower dry then transfer it to the saucepan. Transfer the cauliflower mixture to the buttered baking dish, spreading it in an even layer. Top with the remaining Gruyère, fontina and the Parmesan. Sprinkle the thyme for the final touch.',
-				order: 4
+				order: 3
 			},
 			{
 				step: 'Bake until the mixture begins to bubble and the cauliflower starts to turn golden, about 20 minutes. Allow the casserole to cool for about 5 minutes or so, then serve. Season with flaky salt, if desired.',
-				order: 5
+				order: 4
 			},
 		],
 		tags: ['Main', 'Vegetarian', 'Cheese', 'NYT', 'Cauliflower' ],
@@ -216,19 +216,19 @@ export const RECIPES = [
 		preparation: [
 			{
 				step: 'Heat oven to 200 degrees C and line a sheet pan with parchment paper or foil. Season the salmon fillets well with salt and pepper, and place them on a plate or in a large shallow bowl.',
-				order: 1
+				order: 0
 			},
 			{
 				step: 'In a small bowl, whisk together the maple syrup, miso, rice wine vinegar, soy sauce and garlic. Pour the mixture on top of the salmon and gently massage the marinade all over the fish. Let marinate while the oven comes to temperature.',
-				order: 2
+				order: 1
 			},
 			{
 				step: 'Meanwhile, in a medium bowl, toss together the green beans with the olive oil and season generously with salt and pepper. Lay the salmon fillets on the sheet pan skin side down and spread the green beans out surrounding the fillets.',
-				order: 3
+				order: 2
 			},
 			{
 				step: 'Arrange the salmon skin side down on the sheet pan and bake until the salmon is opaque and the green beans are cooked through, about 12 minutes. Serve with rice.',
-				order: 4
+				order: 3
 			}
 		],
 		tags: ['Main', 'Cheese', 'NYT', 'Salmon' ],
@@ -300,40 +300,160 @@ export const RECIPES = [
 		preparation: [
 			{
 				step: 'Prepare the rolls: In a medium pot, heat the milk and 4 tablespoons butter over medium until the mixture is just warm to the touch (about 95 degrees) and the butter is melted, 1 to 2 minutes.',
-				order: 1
+				order: 0
 			},
 			{
 				step: 'In the bowl of an electric mixer fitted with a dough hook attachment, combine the flour, milk powder, sugar, yeast and salt with 1 egg and the warmed milk mixture. Mix on low for 3 minutes, then increase the speed to medium and mix until the dough is smooth, about 3 minutes.',
-				order: 2
+				order: 1
 			},
 			{
 				step: 'Transfer the dough to a lightly greased bowl, cover with plastic wrap, and let rise in a warm place for 1 1/2 hours. The dough may not double in size, but it should be noticeably puffy.',
-				order: 3
+				order: 2
 			},
 			{
 				step: 'Divide the dough into 12 even pieces (about 71 grams each). Gently round into balls: Cup your hand and fingers around a piece of dough then roll it on your work surface until it forms a tight ball. Repeat with the remaining pieces.',
-				order: 4
+				order: 3
 			},
 			{
 				step: 'In a lightly greased 9-by-13-inch pan, arrange the balls in three rows of four. Cover the rolls with plastic wrap and let rise in a warm place until noticeably puffy, 45 minutes to 1 hour.',
-				order: 5
+				order: 4
 			},
 			{
 				step: 'Heat the oven to 350 degrees. Prepare the everything-bagel seasoning: Toss together the sesame seeds, poppy seeds, dried garlic, dried onion and kosher salt.',
-				order: 6
+				order: 5
 			},
 			{
 				step: 'In a small bowl, beat the remaining egg. Brush the surface of each roll with the beaten egg and sprinkle each generously with everything-bagel seasoning (about 2 teaspoons per roll).',
-				order: 7
+				order: 6
 			},
 			{
 				step: 'Bake the rolls for 10 minutes, then brush the surface of the rolls generously with the melted butter. Bake until the rolls are evenly golden-brown, 15 to 20 minutes. Brush the baked rolls generously with the remaining melted butter. Let cool 10 to 15 minutes before serving. Store leftover rolls at room temperature in a resealable plastic bag. (See tip for reheating instructions).',
-				order: 8
+				order: 7
 			},
 		],
 		tips: 'To reheat, wrap each roll in a lightly damp paper towel, and microwave for 20 to 25 seconds. Unwrap the rolls and serve immediately.',
-		tags: ['Breqd', 'Vegetarian', 'NYT' ],
+		tags: ['Bread', 'Vegetarian', 'NYT' ],
 		last_edited: '',
 		url: 'everything-parker-house-rolls',
+	},
+	{
+		title: 'Roberta’s Pizza Dough',
+		summary: 'This recipe, adapted from Roberta’s, the pizza and hipster haute-cuisine utopia in Bushwick, Brooklyn, provides a delicate, extraordinarily flavorful dough that will last in the refrigerator for up to a week. It rewards close attention to weight rather than volume in the matter of the ingredients, and asks for a mixture of finely ground Italian pizza flour (designated “00” on the bags and available in some supermarkets, many specialty groceries and always online) and regular all-purpose flour. As ever with breads, rise time will depend on the temperature and humidity of your kitchen and refrigerator.',
+		time: 20,
+		yield: '2 12’ pizza',
+		illustration: '../../../assets/images/pizza-daugh.png',
+		ingredients: [
+			{
+				name: '153 grams 00 flour (1 cup plus 1 tablespoon)',
+				order: 0,
+			},
+			{
+				name: '153 grams all-purpose flour (1 cup plus 1 tablespoon and 2 teaspoons)',
+				order: 1,
+			},
+			{
+				name: '8 grams fine sea salt (1 teaspoon)',
+				order: 2,
+			},
+			{
+				name: '2 grams active dry yeast (3/4 teaspoon)',
+				order: 3,
+			},
+			{
+				name: '4 grams extra-virgin olive oil (1 teaspoon)',
+				order: 4,
+			}
+		],
+		preparation: [
+			{
+				step: 'In a large mixing bowl, combine flours and salt.',
+				order: 0
+			},
+			{
+				step: 'In a small mixing bowl, stir together 200 grams (a little less than 1 cup) lukewarm tap water, the yeast and the olive oil, then pour it into flour mixture. Knead with your hands until well combined, approximately 3 minutes, then let the mixture rest for 15 minutes.',
+				order: 1
+			},
+			{
+				step: 'Knead rested dough for 3 minutes. Cut into 2 equal pieces and shape each into a ball. Place on a heavily floured surface, cover with dampened cloth, and let rest and rise for 3 to 4 hours at room temperature or for 8 to 24 hours in the refrigerator. (If you refrigerate the dough, remove it 30 to 45 minutes before you begin to shape it for pizza.)',
+				order: 2
+			},
+			{
+				step: 'To make pizza, place each dough ball on a heavily floured surface and use your fingers to stretch it, then your hands to shape it into rounds or squares. Top and bake.',
+				order: 3
+			}
+		],
+		tags: ['Dough', 'Pizza', 'NYT' ],
+		last_edited: '',
+		url: 'roberta-s-pizza-dough',
+	},
+	{
+		title: 'Tomato-Parmesan Soup',
+		summary: 'What if you could have a tomato soup that was as plush as a cream of tomato but tasted like pure tomato? Enter Parmesan. Simmering tomatoes with a Parmesan rind is like seasoning a bowl of soup with a shaving of cheese 100 times over. It gives the soup an undercurrent of savory fat and salt that only bring out tomato’s best sides. Many specialty groceries sell containers of rinds, but if you can’t find any, stir ½ cup grated Parmesan into the final soup (or cut off the rind of a wedge you’re working through). Rinds will keep in the freezer for forever, so start saving. Pair the soup with Parmesan toast, for dunking, though it’s in no way needed.',
+		time: 45,
+		yield: '4 servings',
+		illustration: '../../../assets/images/as-tomato-parmesan-soup-articleLarge.jpg',
+		ingredients: [
+			{
+				name: '6 tablespoons unsalted butter',
+				order: 0,
+			},
+			{
+				name: '1 yellow onion, chopped',
+				order: 1,
+			},
+			{
+				name: '5 garlic cloves, sliced',
+				order: 2,
+			},
+			{
+				name: '½ teaspoon red-pepper flakes',
+				order: 3,
+			},
+			{
+				name: '2 (28-ounce) cans whole peeled tomatoes',
+				order: 4,
+			},
+			{
+				name: '6 ounces Parmesan rind',
+				order: 5,
+			},
+			{
+				name: '4 (about 3/4-inch thick) slices of baguette',
+				order: 6,
+			},
+			{
+				name: '1 garlic clove, peeled',
+				order: 7,
+			},
+			{
+				name: '¼ cup finely grated Parmesan',
+				order: 8,
+			}
+		],
+		preparation: [
+			{
+				step: 'In a large saucepan or Dutch oven over medium heat, melt the butter. Once it starts bubbling, stir in the onions and sauté until golden, 5 to 7 minutes. Add the garlic, red-pepper flakes, and generous pinches of salt and pepper, and sauté until fragrant, 2 to 3 minutes.',
+				order: 0
+			},
+			{
+				step: 'Stir in the tomatoes and their juices, 1 cup water and the Parmesan rind, and bring to a boil. Turn the heat down to a simmer and cook, stirring occasionally, until the tomatoes are broken down and the mixture has thickened, about 30 minutes. (If you find during your stirring that the Parmesan rind is stuck to the bottom or sides of the pot, pry it off with a wooden spoon.',
+				order: 1
+			},
+			{
+				step: 'Remove the rind from the soup, and let cool slightly. If you’re planning to make the Parmesan toast, heat the oven to 400 degrees.',
+				order: 2
+			},
+			{
+				step: 'Use an immersion blender to purée the soup. (Alternatively, ladle the soup into a blender, and blend until smooth. For a super-smooth soup, run it through a fine-mesh sieve.) Season to taste with salt, pepper and grated Parmesan.',
+				order: 3
+			},
+			{
+				step: 'If you’re making the toast, rub 1 side of each slice of baguette with the garlic clove, then place on a parchment-lined baking sheet. Sprinkle cheese over the top, then bake until golden brown, about 10 minutes. Serve the soup with a toast alongside or on top.',
+				order: 4
+			}
+		],
+		tags: ['Tomato', 'Soup', 'Parmesan', 'NYT' ],
+		last_edited: '',
+		url: 'tomato-parmesan-soup',
 	}
 ]; 
