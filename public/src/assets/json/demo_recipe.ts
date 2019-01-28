d22d018c4e8ac31025a39ab2104db7c87f0fd9ef
Alexandre Nicol <nemokervi@yahoo.fr>
2019-01-29 09:43:31 +1100

    Add 1 recipe

--- public/src/assets/json/demo_recipe.ts
+++ public/src/assets/json/demo_recipe.ts
@@ -622,6 +622,68 @@ export const RECIPES = [
 		url: 'ginger-molasses-cookies',
 	},
 	{
+		title: 'Soba Salad',
+		summary: 'For most people, even experienced cooks, weeknight dinners are not so much a result of careful planning but of what’s on hand — and what can be accomplished fairly quickly. Noodles of all kinds are easy and beloved. But soba noodles, a Japanese staple, are special: they usually take no more than 3 to 4 minutes to cook and, because they’re made from buckwheat, have a slightly firm texture and a nutty flavor. Traditionally, soba are served hot and cold, making them a flexible partner for almost any fresh ingredients you have in the kitchen. In this case, they are paired with shelled frozen edamame, carrots and spinach and a light dressing.',
+		yield: '4 servings',
+		time: 15,
+		illustration: '../../../assets/images/mb-soba-salad-articleLarge.jpg',
+		ingredients: [
+			{
+				name: '3 to 4 ounces soba noodles',
+				order: 0,
+			},
+			{
+				name: '1 carrot, peeled and finely chopped',
+				order: 1,
+			},
+			{
+				name: '2 cups edamame (frozen are fine)',
+				order: 2,
+			},
+			{
+				name: '2 to 3 tablespoons soy sauce',
+				order: 3,
+			},
+			{
+				name: 'Juice of one lime',
+				order: 4,
+			},
+			{
+				name: '2 tablespoons white or light miso',
+				order: 5,
+			},
+			{
+				name: '1 tablespoon mirin or 1 teaspoon sugar, or to taste',
+				order: 6,
+			},
+			{
+				name: '1 10-ounce package fresh spinach, washed and trimmed',
+				order: 7,
+			},
+			{
+				name: '¼ cup chopped scallion ',
+				order: 8,
+			},
+			{
+				name: '1 tablespoon freshly grated ginger ',
+				order: 9,
+			}
+		],
+		preparation: [
+			{
+				step: 'Bring a large pot of water to a boil and salt it. Drop in the noodles and carrot and cook until tender, 2 to 4 minutes; add the edamame for about 15 to 30 seconds just to warm, then drain everything in a colander. Set aside.',
+				order: 0
+			},
+			{
+				step: 'In a large salad bowl, whisk together the soy sauce, lime juice, miso and mirin or sugar. Add spinach, noodles, carrot, edamame, scallion, and toss; then taste and season with salt and pepper as needed. Garnish with grated ginger at the table.',
+				order: 1
+			}
+		],
+		tags: ['Noodle', 'Salad', 'Lunch', 'Dinner', 'NYT' ],
+		last_edited: '',
+		url: 'soba-salad',
+	},
+	{
 		title: 'Breakfast Salad',
 		summary: 'This one was created at Carthage Must Be Destroyed, an airy (and slightly eccentric) Australian-style cafe hidden behind an unmarked entrance in Brooklyn. The chef and owner Amanda Bechara likes to leave the lettuce leaves whole to make it easier to eat with your fingers. (You can prepare the vegetables the day before, and skip marinating the feta if you must.) This would also make a lovely lunch.',
 		yield: '2 servings',
